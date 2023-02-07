dc6d2ee30aaa7e2501144efc9e46faa75705c84e
taposhsingha <57071916+taposhsingha@users.noreply.github.com>
2023-02-07 21:51:35 +0600

    done

---
+++ app/src/main/java/com/example/mediafiles/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.mediafiles
+
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mediaPlayer: MediaPlayer
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mediaPlayer = MediaPlayer.create(this,R.raw.applauding)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            mediaPlayer.start()
+        }
+    }
+}
